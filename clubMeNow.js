5c79396bfdb0440ac41de2af7718d3b0f086cc5d
Rayboon <70655873+Rayboon@users.noreply.github.com>
2020-10-26 12:26:31 -0400

    Update clubMeNow.js

--- clubMeNow.js
+++ clubMeNow.js
@@ -130,7 +130,7 @@ function resetAllClubDistances() {
 function displayAbout() {
 	alert("ClubMeNow version 1.0.0");
 	// your code here
-	window.location.href = "clubAbout.html";
+	//window.location.href = "clubAbout.html";
 }
 
 // navigate to "Penalty Info" screen
